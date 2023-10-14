99aa6ea2068fcebe3a7bdb0948bce4416a0b273b
Long Lane <tech@long-lane.co.uk>
2023-10-14 13:24:12 +0100

    add footer image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import sootSprite from '../assets/img/soot_sprite.png'
 export const Footer = () => {
   return (
     <footer>
-        <p> &copy; {(new Date).getFullYear()} <a href='https://crlarrea.github.io' target='_blank'>crlarrea</a>
-        </p>
+        <img src={sootSprite}/>
+      <p>
+        
+        &copy; {new Date().getFullYear()}{" "}
+        <a href="https://crlarrea.github.io" target="_blank">
+          crlarrea
+        </a>
+      </p>
     </footer>
-  )
-}
+  );
+};
